be5bd1e7234f1f98b2fbfe83c88357cd0fd79552
David Wright <720482+dkwgit@users.noreply.github.com>
2021-12-08 09:40:24 -0600

    After session with Tim

--- Deploy-CFStack.ps1
+++ Deploy-CFStack.ps1
@@ -2,8 +2,19 @@ function Deploy-CFStack
 {
     param(
       [Parameter(Mandatory=$true)][string]$FileName,
-      [Parameter(Mandatory=$true)][string]$StackName
+      [Parameter(Mandatory=$true)][string]$StackName,
+      [Parameter(Mandatory=$false)][string]$ParameterKey="",
+      [Parameter(Mandatory=$false)][string]$ParameterValue="",
+      [Parameter(Mandatory=$false)][string]$ParameterKey2="",
+      [Parameter(Mandatory=$false)][string]$ParameterValue2=""
+
     )
 
-    aws cloudformation create-stack --stack-name $StackName --template-body file://$FileName
+    if ($ParameterKey -ne "")
+    {
+      aws cloudformation create-stack --stack-name $StackName --template-body file://$FileName --parameters "ParameterKey=$ParameterKey,ParameterValue=$ParameterValue" "ParameterKey=$ParameterKey2,ParameterValue=$ParameterValue2" 
+    }
+    else {
+      aws cloudformation create-stack --stack-name $StackName --template-body file://$FileName
+    }
 }
